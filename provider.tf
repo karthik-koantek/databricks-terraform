4c75fb89e2bd9f79c310aab6aa81ba8f434bc513
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-07 11:23:28 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,6 +12,8 @@ terraform {
 
 provider "aws" {
   region = var.region
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 // initialize provider in "MWS" mode to provision new workspace
