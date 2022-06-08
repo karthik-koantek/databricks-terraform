767ec67c00aa1f4fe7fee5209129877b1e6f4c94
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-07 23:13:06 -0400

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -1,7 +1,3 @@
-resource "time_sleep" "wait" {
-  depends_on = [aws_iam_role.cross_account_role]
-  create_duration = "180s"
-}
 
 resource "databricks_mws_workspaces" "this" {
   provider        = databricks.mws
@@ -9,7 +5,7 @@ resource "databricks_mws_workspaces" "this" {
   aws_region      = var.region
   workspace_name  = local.prefix
 
-  credentials_id           = databricks_mws_credentials.this.credentials_id
+  credentials_id           = "test-databricks-clr"
   storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
   network_id               = databricks_mws_networks.this.network_id
 
