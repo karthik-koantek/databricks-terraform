2d8eb0ba671059b571c238710dd1373a7d8321ed
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-09 23:44:11 -0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -28,7 +28,7 @@ resource "databricks_mws_credentials" "this" {
   account_id       = var.databricks_account_id
   role_arn         = aws_iam_role.cross_account_role.arn
   credentials_name = "${local.prefix}-creds"
-  depends_on       = [aws_iam_role_policy.this,time_sleep.wait_240_seconds]
+  depends_on       = [aws_iam_role_policy.this]
   
  
 }
