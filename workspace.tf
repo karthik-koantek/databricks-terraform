d4549fe40d3076bf79e53adfc5ad260818298622
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-11 18:52:46 -0400

    Create workspace.tf

---
+++ workspace.tf
@@ -0,0 +1,23 @@
+resource "databricks_mws_workspaces" "this" {
+  provider       = databricks.mws
+  account_id     = var.databricks_account_id
+  aws_region     = var.region
+  workspace_name = local.prefix
+
+  credentials_id           = "27ac9f30-2d07-41c3-b4c5-97912b9cfed9"
+  storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
+  network_id               = databricks_mws_networks.this.network_id
+
+  token {
+    comment = "Terraform"
+  }
+}
+
+output "databricks_host" {
+  value = databricks_mws_workspaces.this.workspace_url
+}
+
+output "databricks_token" {
+  value     = databricks_mws_workspaces.this.token[0].token_value
+  sensitive = true
+}
