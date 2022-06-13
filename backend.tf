8ebd1f818892a55226ec90e295a630e93856529a
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-13 17:51:24 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -7,7 +7,6 @@ terraform {
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
-     access_key = "${var.access_key}"
-     secret_key = "${var.secret_key}"
+  
   }
 }
