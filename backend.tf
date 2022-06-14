65284d1df0af79323372d762ccc1fb5831f39771
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-13 21:03:04 -0400

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform_state_koan"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
