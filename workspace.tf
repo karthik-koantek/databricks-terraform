29a853b3e72a453a5cca28829414c346e3f0d3f6
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-14 17:11:38 -0400

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -4,7 +4,8 @@ resource "databricks_mws_workspaces" "this" {
   aws_region     = var.region
   workspace_name = local.prefix
 
-  credentials_id           = "27ac9f30-2d07-41c3-b4c5-97912b9cfed9"
+  #credentials_id           = "27ac9f30-2d07-41c3-b4c5-97912b9cfed9"
+  credentials_id = "1d6a5beb-c65e-4d7b-b589-86cd5c019a3e"
   storage_configuration_id = databricks_mws_storage_configurations.this.storage_configuration_id
   network_id               = databricks_mws_networks.this.network_id
 
