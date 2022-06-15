4c3c97aa38a531b1ba8669ae43a4b883e8746b10
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-15 19:22:24 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,6 @@ resource "databricks_mws_networks" "this" {
   account_id         = var.databricks_account_id
   network_name       = "${local.prefix}-network"
   security_group_ids = ["sg-0cd8b19b5858e3d61"]
-  subnet_ids         = ["subnet-044feb979292a7a22", "subnet-07105e3417b44b644", "subnet-00fc366e61f1cfb1f", "subnet-0cf1f262026daa1d1"]
+  subnet_ids         = ["subnet-044feb979292a7a22","subnet-0cf1f262026daa1d1"]
   vpc_id             = "vpc-005c915de1b5a8454"
 }
