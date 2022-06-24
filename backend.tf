56a94874e01e845808909d521ddacc71a9f3d55c
karthik-koantek <106896843+karthik-koantek@users.noreply.github.com>
2022-06-24 11:46:46 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     # Replace this with your bucket name!
     bucket         = "terraform-state-koan"
-    key            = "global/s3/terraform.tfstate"
+    key            = "global/s3/terraform1.tfstate"
     region         = "eu-central-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
